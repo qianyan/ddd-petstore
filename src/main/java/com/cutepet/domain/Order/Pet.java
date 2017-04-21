851a006feb179ea2b0214672f5d36ed921a0bc19
林帆 <flin@thoughtworks.com>
2017-04-21 17:38:05 +0800

    Add order apis

--- src/main/java/com/cutepet/domain/Order/Pet.java
+++ src/main/java/com/cutepet/domain/Order/Pet.java
@@ -1,22 +1,56 @@
 package com.cutepet.domain.Order;
 
 import com.cutepet.domain.Common.PaymentMethod;
+import com.cutepet.domain.Common.PetType;
 
-public class Pet {
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+public class Pet implements Serializable {
 
     private String name;
     private String color;
+    private PetType type;
     private PaymentMethod paymentMethod;
+
+    public Pet(String name, String color, PetType type, PaymentMethod paymentMethod) {
+        this.name = name;
+        this.color = color;
+        this.type = type;
+        this.paymentMethod = paymentMethod;
+    }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getColor() {
         return color;
     }
 
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public PetType getType() {
+        return type;
+    }
+
+    public void setType(PetType type) {
+        this.type = type;
+    }
+
     public PaymentMethod getPaymentMethod() {
         return paymentMethod;
     }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
 }
